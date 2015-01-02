0bd7983c1e551407e36a2702d7bf3ba8e5552caf
Charles Severance <csev@umich.edu>
2015-01-02 11:03:19 -0500

    Reached MVP with the content item support for Canvas.

---
+++ register.php
@@ -0,0 +1,8 @@
+<?php
+
+$REGISTER_LTI2 = array(
+"name" => "Participant Map using Google",
+"short_name" => "Course Map",
+"description" => "This provides a simple Google map for a course where participants can indicate their location and control how much information they release to those viewing the map."
+);
+
