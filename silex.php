54e3b4e15c8785b72c3d8a980abe607fe49d879d
Charles Severance <csev@umich.edu>
2017-04-10 00:08:28 -0400

    Improve translation example

--- silex.php
+++ silex.php
@@ -2,11 +2,8 @@
 require_once "../config.php";
 
 use \Tsugi\Core\LTIX;
-use \Tsugi\Core\Settings;
-use \Tsugi\Util\Net;
 
 $launch = LTIX::requireData();
-// if ( count($_POST) > 0 ) { var_dump($launch); die(); }
 $app = new \Tsugi\Silex\Application($launch);
 $app['debug'] = true;
 
