8068d53b94f808004261a69321431aabe55db1c8
Charles Severance <csev@umich.edu>
2017-12-30 21:51:25 -0500

    Pull in new registration / store bits.

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 require_once "../config.php";
 
+if ( \Tsugi\Core\Tool::handleConfig() ) return;
+
 use \Tsugi\Core\LTIX;
 
 $launch = LTIX::requireData();
--- register.php
+++ register.php
@@ -4,5 +4,28 @@ $REGISTER_LTI2 = array(
 "name" => "Simple Attendance Tool",
 "FontAwesome" => "fa-server",
 "short_name" => "Attendance Tool",
-"description" => "This is a simple attendance tool that allows the instructor to set a code and the students enter the code."
+"description" => "This is a simple attendance tool that allows the instructor to set a code and the students enter the code.",
+    // By default, accept launch messages..
+    "messages" => array("launch"),
+    "privacy_level" => "name_only",  // anonymous, name_only, public
+    "license" => "Apache",
+    "languages" => array(
+        "English", "Spanish"
+    ),
+    "source_url" => "https://github.com/tsugitools/attend",
+    // For now Tsugi tools delegate this to /lti/store
+    "placements" => array(
+        /*
+        "course_navigation", "homework_submission",
+        "course_home_submission", "editor_button",
+        "link_selection", "migration_selection", "resource_selection",
+        "tool_configuration", "user_navigation"
+        */
+    ),
+    "screen_shots" => array(
+        "store/screen-01.png",
+        "store/screen-02.png",
+        "store/screen-03.png"
+    )
+
 );
